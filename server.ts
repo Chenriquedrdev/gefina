a6af24f09250d920a3154c86d45647061d26031a
Claudio Henrique <Claudiohenriquedr@gmail.com>
2026-09-25 16:48:15 -0300

    adicionar servidor da interface de programação com recurso de saude
    Closes #2

---
+++ server.ts
@@ -0,0 +1,12 @@
+import { createServer } from 'node:http';
+
+createServer(function (request, response) {
+    if (request.url !== '/api/health'){
+        response.writeHead(404, { 'content-type': 'application/json' });
+        response.end(JSON.stringify({ message: 'Recurso não encontrado' }));
+        Returne;
+    }
+
+    response.writeHead(200, { 'content-type': 'application/json' });
+    response.end(JSON.stringify({ status: 'OK' }));;
+}).listen(3000);
